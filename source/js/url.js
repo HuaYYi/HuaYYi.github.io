364073057b4caa1621d6e3b964ecc300538bbeee
HuaYYi <248463128@qq.com>
2021-09-12 13:52:53 +0800

    123

--- source/js/url.js
+++ source/js/url.js
@@ -1,3 +1,5 @@
+//添加默认图，img加载失败时使用
+var ddd ="this.src='source/image/sdcnav-8-2.png';this.onerror=null"
 var sss =false
 //外层循环，对target_url进行循环
 for (var i = 0; i < target_url.length; i++){
@@ -10,7 +12,7 @@ for (var i = 0; i < target_url.length; i++){
             //创建一个元素节点li
             var li=document.createElement("li")
             //结构组合
-            var li_a= li_a = "<a href= " + target_url[i].href + " target='_blank'>" + "<img src=" + target_url[i].img + " ait='图'>" + "<h3>" + target_url[i].title + "</h3>" + "<p>" + target_url[i].describe + "</p>" + "</a>";
+            var li_a= li_a = "<a href= " + target_url[i].href + " target='_blank'>" + "<img src=" + target_url[i].img + " ait='图' onerror=" + ddd + ">" + "<h3>" + target_url[i].title + "</h3>" + "<p>" + target_url[i].describe + "</p>" + "</a>";
             //把结构组合放进li元素里
             li.innerHTML=li_a
             //把li放进此时的ul里
@@ -24,7 +26,7 @@ for (var i = 0; i < target_url.length; i++){
     };
     if (!sss) {
         var div=document.createElement("div");
-        var div_n= div_n= "<h2>" + target_url[i].kind + "</h2>" + "<ul name=" + target_url[i].kind + ">" + "<li>" + "<a href= " + target_url[i].href + " target='_blank'>" + "<img src=" + target_url[i].img + "  ait='图'>" + "<h3>" + target_url[i].title + "</h3>" + "<p>" + target_url[i].describe + "</p>" + "</a>" + "</li>" + "<p>" + "</p>" + "</ul>";
+        var div_n= div_n= "<h2>" + target_url[i].kind + "</h2>" + "<ul name=" + target_url[i].kind + ">" + "<li>" + "<a href= " + target_url[i].href + " target='_blank'>" + "<img src=" + target_url[i].img + "  ait='图' onerror=" + ddd + ">" + "<h3>" + target_url[i].title + "</h3>" + "<p>" + target_url[i].describe + "</p>" + "</a>" + "</li>" + "<p>" + "</p>" + "</ul>";
         div.innerHTML=div_n;
         var list =document.getElementById("ff");
         // 向上插入
